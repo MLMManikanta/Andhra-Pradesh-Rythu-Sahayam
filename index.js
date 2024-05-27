0ae8cfbef19804d415994a18a35bce49dc8aed7d
mlmmanikanta <mlmmanikanta@outlook.com>
2024-05-27 21:26:37 +0530

    Skip to main created

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var skipLink = document.querySelector(".skip-link");
+    if (skipLink) {
+        skipLink.style.display = "none";
+    }
+});
